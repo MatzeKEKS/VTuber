fb85e23bf2fe38a3a6e655bff3edaa6f112deecd
Matze <msondershaus@kabelmail.de>
2025-07-25 15:08:02 +0200

    1. Desing verbesserungen

--- VTuber/src/app/app.component.ts
+++ VTuber/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {trigger, transition, style, animate} from '@angular/animations';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  animations: 
+  [
+    trigger('fadeIn', 
+      [
+        transition(':enter', 
+          [
+            style({ opacity: 0, transform: 'translateY(30px)' }),
+            animate('1500ms ease-in', 
+              style({ opacity: 1, transform: 'translateY(0)' }))
+          ]
+        )
+      ]),
+      
+    trigger('fadeInDelayed', 
+      [
+        transition(':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(30px)' }),
+            animate('800ms 1s ease-out',
+              style({ opacity: 1, transform: 'translateY(0)' }))
+          ]
+        )
+      ])
+  ]
 })
+
 export class AppComponent {
   title = 'VTuber';
 }
