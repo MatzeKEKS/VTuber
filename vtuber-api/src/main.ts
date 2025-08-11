07d441c3cc3772a74c3a807484339f21f33b1c06
Matze <msondershaus@kabelmail.de>
2025-08-11 19:41:27 +0200

    Api

---
+++ vtuber-api/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:4200',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+}
+void bootstrap();
